33c597bd94ffa94f6b5a2c06aff66016315bf5f8
EC2 Default User <ec2-user@ip-172-31-38-245.ap-northeast-1.compute.internal>
2023-10-04 00:41:20 +0000

    “routing”

--- README.md
+++ README.md
@@ -1,24 +1,32 @@
-# README
+# <!--Souvenir-->
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+## サイト概要
+### サイトテーマ
+今までに贈ったプレゼントの見返す為の、記録用アプリケーションがテーマ
 
-Things you may want to cover:
+### テーマを選んだ理由
+家族や恋人にプレゼントを贈ろうと考えていた際に、以前は何を贈ったかを忘れてしまった事がありました。その経験から画像・商品名・購入金額・購入日時・購入場所等が記録できるアプリケーションを作ろうと考えたからです。<br>また自分自身の記録用としてだけでなく、他ユーザーのレビューも閲覧をできるようにして、何を贈ろうか悩んでいるユーザーが一考できるようにもして、自分と同じような悩みを持つ方々の力になりたいと思い作成しました。
 
-* Ruby version
+### ターゲットユーザ
+- 以前の記念日に、何をプレゼントをしたか思い出したい人<br>
+- 家族や恋人へのプレゼントに、何を贈るか悩んでいる方々
 
-* System dependencies
+### 主な利用シーン
+- クリスマスや誕生日等の記念日に向けて、プレゼントを準備する時<br>
+- 家族や恋人との思い出を見返したい時
 
-* Configuration
+## 設計書
 
-* Database creation
 
-* Database initialization
+## 開発環境
+- OS：Linux(CentOS)
+- 言語：HTML,CSS,JavaScript,Ruby,SQL
+- フレームワーク：Ruby on Rails
+- JSライブラリ：jQuery
+- IDE：Cloud9
 
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+## 使用素材
+- 外部サービスの画像素材・音声素材を使用した場合は、必ずサービス名とURLを明記してください。
+- アプリケーションの実装に使用したgem/bootstrapのリファレンスなどの記載は不要です。
+- 使用しない場合は、使用素材の項目をREADMEから削除してください。
+折りたたむ
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,42 @@
 Rails.application.routes.draw do
-  
-  # 顧客用
-# URL /users/sign_in ...
-devise_for :users, skip: [:passwords], controllers: {
-  registrations: "user/registrations",
-  sessions: 'user/sessions'
-}
+root to: 'user/homes#top'
+get '/search', to: 'searches#search'
+get 'tagsearches/search', to: 'tagsearches#search'
+
+#管理者用
+namespace :admin do
+  root to: "homes#top"
+  resources :customers, only: [:index, :show, :edit, :update]
+  resources :genres, only: [:index, :edit, :create, :update]
+end
 
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+
+#顧客用
+namespace :user do
+    resources :items, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :item_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+end
+
+# 顧客用
+# URL /users/sign_in ...
+devise_for :users, skip: [:passwords], controllers: {
+  registrations: "user/registrations",
+  sessions: 'user/sessions'
+}
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
